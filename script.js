23563da14214680125ee8f50481eca5184a40c04
Emil Leedham <you@example.com>
2026-01-04 18:44:23 +1100

    setTimeout

--- script.js
+++ script.js
@@ -1,79 +1,84 @@
-"use strict";
-
 const quoteContainer = document.getElementById("quote-container");
 const quoteText = document.getElementById("quote");
-const quoteAuthor = document.getElementById("author");
-const twitterButton = document.getElementById("twitter");
-const newQuoteButton = document.getElementById("new-quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
 const loader = document.getElementById("loader");
+
+// -----> If you're using the localQuotes array you can remove apiQuotes variable
 
 let apiQuotes = [];
 
-//Show Loading
+// Loading Spinner Shown
 function loading() {
   loader.hidden = false;
   quoteContainer.hidden = true;
 }
 
-//Hide Loading
+// Remove Loading Spinner
 function complete() {
   quoteContainer.hidden = false;
   loader.hidden = true;
 }
 
-//Show new Quote
+// Show New Quote
 function newQuote() {
   loading();
-  //Pick a random quote
-  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-  // console.log(quote);
-  quoteAuthor.textContent = quote.author;
-  quoteText.textContent = quote.text;
-  //Check if Author: Firstname Lastname is blank and replace it with 'unknown'
-  if (!quote.author) {
-    quote.author = "Unknown";
-  }
-  //Check Quote Length to determine styling
-  if (quote.text.length > 120) {
-    quoteText.classList.add("long-quote");
-  } else {
-    quoteText.classList.remove("long-quote");
-  }
-  //Set Quote, Hide Loader
-  quoteText.textContent = quote.text;
-  quoteAuthor.textContent = quote.author;
-  complete();
+  // Use setTimeout to delay the process by 1000ms (1 second)
+  setTimeout(() => {
+    // -----> Only use one of the following const quote statements
+
+    // Pick a random quote from apiQuotes array
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+
+    // Pick a random quote from localQuotes array
+    // const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
+
+    // Check if Author field is blank and replace it with 'Unknown'
+    if (!quote.author) {
+      authorText.textContent = "Unknown";
+    } else {
+      authorText.textContent = quote.author;
+    }
+    // Check Quote length to determine styling
+    if (quote.text.length > 120) {
+      quoteText.classList.add("long-quote");
+    } else {
+      quoteText.classList.remove("long-quote");
+    }
+    // Set Quote, Hide Loader
+    quoteText.textContent = quote.text;
+    complete();
+  }, 1000);
 }
 
-//Get Quote From API
-async function getQuote() {
+// -----> If you're using the localQuotes array you can remove getQuotes() function
+
+// Get Quotes From API
+async function getQuotes() {
   loading();
-  //   const proxyUrl = "http://cors-anywhere.herokuapp.com/";
   const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
   try {
     const response = await fetch(apiUrl);
     apiQuotes = await response.json();
     newQuote();
   } catch (error) {
-    //catch error
+    // Catch Error Here
   }
 }
+
 // Tweet Quote
 function tweetQuote() {
-  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${quoteAuthor.textContent}`;
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.innerText} - ${authorText.innerText}`;
   window.open(twitterUrl, "_blank");
 }
 
-//Event Listeners
-newQuoteButton.addEventListener("click", newQuote);
-twitterButton.addEventListener("click", tweetQuote);
+// Event Listeners
+newQuoteBtn.addEventListener("click", newQuote);
+twitterBtn.addEventListener("click", tweetQuote);
 
-//On Load; can't be invoked/called before function declaration due to hoisting.
-getQuote();
+// On Load
+getQuotes();
 
-// Sample Requests,There is an alternative option for this project that does not require an API key - https://zenquotes.io/
-// https://zenquotes.io/api/quotes - Generate a JSON array of 50 random quotes on each request
-
-// https://zenquotes.io/api/today - Generate the quote of the day on each request
-
-// https://zenquotes.io/api/random - Generate a random quote on each request
+// -----> If using API, run getQuotes(), if not run newQuote() instead
+// newQuote();
